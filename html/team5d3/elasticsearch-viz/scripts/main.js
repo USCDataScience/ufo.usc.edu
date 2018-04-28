a26f5ccdfac1cfa22ba13950cd049fa8009c0f94
Fulcrum123 <anuragmantri93@gmail.com>
2018-04-28 15:23:17 -0700

    Elasticsearch Viz added.

--- html/team5d3/elasticsearch-viz/scripts/main.js
+++ html/team5d3/elasticsearch-viz/scripts/main.js
@@ -1,6 +1,6 @@
 define(['scripts/d3.min', 'scripts/elasticsearch'], function (d3, es) {
     "use strict";
-    
+
     var client = new elasticsearch.Client({
         host: 'localhost:9200',
         log: 'trace',
@@ -17,7 +17,7 @@ define(['scripts/d3.min', 'scripts/elasticsearch'], function (d3, es) {
             "aggs": {
                 "shapes": {
                   "terms": {
-                    "field": "shape.keyword",
+                    "field": "shape",
                     "size": 5
                   }
                 }
@@ -29,10 +29,12 @@ define(['scripts/d3.min', 'scripts/elasticsearch'], function (d3, es) {
         //D3 code goes here.
         var shapes = resp.aggregations.shapes.buckets;
         // d3 donut chart
-        var width = 600,
-            height = 300,
-            radius = Math.min(width, height) / 2;
-        var color = ['#131a00','#394d00','#608000', '#86b300', '#ace600', '#c6ff1a','#ccff33','#d2ff4d','#dfff80','#f2ffcc'];
+        var width = 800,
+            height = 500,
+            radius = Math.min(width, height) / 2,
+            labelr = radius-50;
+        //var color = ['#131a00','#394d00','#608000', '#86b300', '#ace600', '#c6ff1a','#ccff33','#d2ff4d','#dfff80','#f2ffcc'];
+        var color = ['#ff7f0e', '#d62728', '#2ca02c', '#1f77b4','#663300'];
         var arc = d3.arc()
             .outerRadius(radius - 60)
             .innerRadius(120);
@@ -53,9 +55,27 @@ define(['scripts/d3.min', 'scripts/elasticsearch'], function (d3, es) {
             .attr("d", arc)
             .style("fill", function (d, i) { return color[i]; });
         g.append("text")
-            .attr("transform", function (d) { return "translate(" + arc.centroid(d) + ")"; })
+            .attr("transform", function (d) { 
+                var c = arc.centroid(d),
+                x = c[0],
+                y = c[1],
+                // pythagorean theorem for hypotenuse
+                h = Math.sqrt(x*x + y*y);
+                return "translate(" + (x/h * labelr) +  ',' +
+                (y/h * labelr) +  ")"; 
+            })
+                //return "translate(" + arc.centroid(d) + ")"; })
             .attr("dy", ".35em")
-            .style("text-anchor", "start")
+            .style("text-anchor", function(d) {
+                // are we past the center?
+                return (d.endAngle + d.startAngle)/2 > Math.PI ?
+                    "end" : "start";
+            })
+            // .attr("text-anchor", function(d) {
+            //     // are we past the center?
+            //     return (d.endAngle + d.startAngle)/2 > Math.PI ?
+            //         "end" : "start";
+            // })
             .style("fill", "black")
             .text(function (d) { return d.data.key; });
     });
